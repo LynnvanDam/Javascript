2db8924b1c437fb06335b26cd78236b40dadbde0
LynnvD <lvandam@roc-dev.com>
2021-03-01 13:28:14 +0100

    002 Javascript, headers en plaatjes aangepast

---
+++ vulmetjs.js
@@ -0,0 +1,13 @@
+
+/* - headers - */
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel"
+
+document.getElementById("subkop").innerText = "Dit is de subkop"
+/* - einde headers - */
+
+
+/* - plaatjes - */
+document.getElementById("pic1").src = "img/pic1.jpg"
+
+document.getElementById("pic2").src = "img/pic2.jpg"
+/* - einde plaatjes - */
